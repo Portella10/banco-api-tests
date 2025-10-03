2c41bc22f44371c69d923b95a0ee5b40b27479b6
João Portella <{jvportella.m@gmail.com}>
2025-10-03 15:50:54 -0300

    Iniciando um projeto

---
+++ test/login.test.js
@@ -0,0 +1,19 @@
+const request = require("supertest");
+const { exepct, expect } = require("chai");
+
+describe("Login", () => {
+  describe("POST /Login", () => {
+    it("Deve retornar status 200 com o token do tipo string ", async () => {
+      const response = await request("http://localhost:3000")
+        .post("/login")
+        .set("Content-Type", "application/json")
+        .send({
+          username: "julio.lima",
+          senha: "123456",
+        });
+
+      expect(response.status).to.equal(200);
+      expect(response.body.token).to.be.a("string");
+    });
+  });
+});
