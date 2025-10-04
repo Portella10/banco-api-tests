b536d6c210d977aa80eef719fca5dec398536383
João Portella <{jvportella.m@gmail.com}>
2025-10-04 13:46:35 -0300

    Adicionando helpers para autenticacao do usuario

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,19 +1,12 @@
 const request = require("supertest");
 const { expect } = require("chai");
 require("dotenv").config();
+const { obterToken } = require("../helpers/autenticacao");
 
 describe("Transferencia", () => {
   describe("POST /transferencia", () => {
     it("Deve retornar status 200 após colocar um valor igual ou maior que R$10,00", async () => {
-      const responseLogin = await request(process.env.BASE_URL)
-        .post("/login")
-        .set("Content-Type", "application/json")
-        .send({
-          username: "julio.lima",
-          senha: "123456",
-        });
-
-      const token = responseLogin.body.token;
+      const token = await obterToken("julio.lima", "123456");
 
       const response = await request(process.env.BASE_URL)
         .post("/transferencias")
@@ -29,15 +22,7 @@ describe("Transferencia", () => {
       expect(response.status).to.equal(201);
     });
     it("Deve retornar status 422 após colocar um valor menor que R$10,00", async () => {
-      const responseLogin = await request(process.env.BASE_URL)
-        .post("/login")
-        .set("Content-Type", "application/json")
-        .send({
-          username: "julio.lima",
-          senha: "123456",
-        });
-
-      const token = responseLogin.body.token;
+      const token = await obterToken("julio.lima", "123456");
 
       const response = await request(process.env.BASE_URL)
         .post("/transferencias")
